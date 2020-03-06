328bcac6d31cb878fe671bfc4bfd59957ac82be9
horelvis <hcastillo.mendoza@gmail.com>
2020-03-06 10:11:49 +0100

    added IOS base64 native

--- src/ios/Base64Plugin.m
+++ src/ios/Base64Plugin.m
@@ -11,7 +11,26 @@
 
 - (void)encodeFile:(CDVInvokedUrlCommand*)command
 {
-  NSArray *params = command.arguments;
+   NSArray *params = command.arguments;
+
+   NSString *path = params[0];
+
+   CDVPluginResult* pluginResult = nil;
+
+   NSData *fileData = [NSData dataWithContentsOfURL:[NSURL URLWithString:path]];
+
+   NSString *encodeBase64 = [fileData base64EncodedStringWithSeparateLines:false];
+
+   if (fileData != nil && encodeBase64) {
+       pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:encodeBase64];
+   } else {
+       pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"filePath was null"];
+   }
+
+   [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+
+
 
 @end
